2940853a5793f43b520841f5fcdff1bcc14e97f3
Guilherme Ventura <guilherme@visionline.com.br>
2018-03-02 18:03:02 -0300

    ajustando navegação

--- js/sliderVentura.js
+++ js/sliderVentura.js
@@ -1,10 +1,11 @@
 config = {
 	centerW : false,
-	slidesToShow : 1,
+	slidesToShow : 2,
 	slidesToScroll: 1,
 	navElement: ''
 }
 
+//Função para gerar slides
 var SliderVentura = function(selector, config){
 	this.init(selector, config);
 };
@@ -16,37 +17,38 @@ SliderVentura.prototype.init = function (selector, config) {
 	track.classList.add('track-ventura');
 	
 	var slides = document.querySelector(selector).children;
-	
-	slider.style.height = slides[0].offsetHeight + 'px';	
-	var slideW = (slider.offsetWidth/config.slidesToShow);
+			
+	var slideW = (slider.clientWidth/config.slidesToShow);
 		
 	for(var i = 0; i < slides.length; i++){		
-		slides[i].classList.add('slide-ventura')
-		slides[i].style.width = (slider.offsetWidth/config.slidesToShow) + 'px';
+		slides[i].classList.add('slide-ventura')		
 		slides[i].style.position = 'absolute';
-		slides[i].style.top = '0';
-		slides[i].style.left = (i * slider.offsetWidth)/config.slidesToShow + 'px';
-		slides[i].addEventListener('drag', function(e){
-			console.log(e);
-			track.style.marginLeft = -e.pageX + (-1 *(currentSlide * slideW) * config.slidesToScroll) + 'px';
-		})
+		slides[i].style.top = '0';				
 	}
-	
+			
 	slides = document.querySelectorAll(selector + ' .slide-ventura');
+	slider.style.height = slides[0].offsetHeight + 'px';
 	
-	var qtdSlides = Math.floor(document.querySelectorAll(selector + ' .slide-ventura:not(.slide-ventura-clone)').length/config.slidesToScroll);	
+	var qtdSlides = Math.ceil(document.querySelectorAll(selector + ' .slide-ventura:not(.slide-ventura-clone)').length);	
 	
 	slider.appendChild(track);
 	
+	track.addEventListener('drag', function(e){
+		console.log(e);
+		track.style.marginLeft = e.pageX + (-1 *(currentSlide * slideW) * config.slidesToScroll) + 'px';
+	})
+	
 	for (var i = 0; i < slides.length; i++){
-		track.appendChild(slides[i]);
+		track.appendChild(slides[i]);		
+		slideW = (slider.clientWidth/config.slidesToShow);
+		slides[i].style.width = slideW + 'px';
+		slides[i].style.left = (i * slider.clientWidth)/config.slidesToShow + 'px';		
 	}
 	
 	this.next = function(){
 		if (qtdSlides-1 == currentSlide)
 			return;
-		currentSlide++;
-		slider.style.height = slides[currentSlide].offsetHeight + 'px';
+		currentSlide++;		
 		track.style.marginLeft = (-1 *(currentSlide * slideW) * config.slidesToScroll) + 'px';		
 		slider.style.height = slides[currentSlide].offsetHeight + 'px';
 	}
@@ -66,10 +68,6 @@ SliderVentura.prototype.init = function (selector, config) {
 		currentSlide = pos;
 		slider.style.height = slides[currentSlide].offsetHeight + 'px';
 		track.style.marginLeft = (-1 *(currentSlide * slideW) * config.slidesToScroll) + 'px';
-	}
-	
-	this.getCurrent = function(){
-		return currentSlide;
 	}
 	
 	this.getCurrent = function(){
@@ -102,7 +100,7 @@ SliderVentura.prototype.init = function (selector, config) {
 	
 
 window.addEventListener('load', function(){
-//	var slider = new SliderVentura('#slider', config);
+	var slider = new SliderVentura('#slider', config);
 })
 
 
